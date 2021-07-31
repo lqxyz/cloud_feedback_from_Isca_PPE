dd3ba3d06c227023e813c9c2e39628df5842fccf
Qun Liu <liuqunxyz@gmail.com>
2021-07-31 17:16:58 +0100

    add downloaded cloud kernel

---
+++ download_cloud_rad_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+outdir=./inputs/
+echo "Download cloud radiative kernel data from Zelinka et al (2012)"
+
+wget -c -N --directory-prefix=$outdir https://github.com/mzelinka/cloud-radiative-kernels/raw/master/data/cloud_kernels2.nc
+wget -c -N --directory-prefix=$outdir https://github.com/mzelinka/cloud-radiative-kernels/raw/master/data/obs_cloud_kernels3.nc
+
+echo Done! Cloud kernel data saved in $outdir
+
