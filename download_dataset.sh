c8c0d65da9d4e2007cae3cd94e14ded736f97460
Qun Liu <liuqunxyz@gmail.com>
2021-08-01 10:44:06 +0100

    delete symbol links before download

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 outdir=./inputs/
-echo "Download cloud radiative kernel data from Zelinka et al (2012)"
 
+# # Delete the symbol link files before downloading 
+# find $outdir -type l -delete
+
+echo "Download cloud radiative kernel data from Zelinka et al (2012)"
 # https://stackoverflow.com/questions/4944295/skip-download-if-files-already-exist-in-wget
 wget -c -N --directory-prefix=$outdir https://github.com/mzelinka/cloud-radiative-kernels/raw/master/data/cloud_kernels2.nc
 wget -c -N --directory-prefix=$outdir https://github.com/mzelinka/cloud-radiative-kernels/raw/master/data/obs_cloud_kernels3.nc
-
 echo "Cloud kernel data saved in $outdir"
 
 echo "Download ISCCP observation data" 
