6f7e4298ca65e83c1116417f6a5839f9ed7b860f
Qun Liu <liuqunxyz@gmail.com>
2021-08-02 18:44:44 +0100

    Add scripts for cloud controling factor analysis

---
+++ scripts/ccf_analysis_cu_sc_metric.py
@@ -0,0 +1,417 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import numpy as np
+import xarray as xr
+import pandas as pd
+import os
+import sys
+sys.path.append('../../scripts')
+from analysis_functions import add_datetime_info, detrend_dim
+from isca_cre_cwp import (calc_toa_cre_for_isca, get_gm,
+    calc_total_cwp_for_isca, calc_low_mid_high_cwp_for_isca)
+import warnings
+warnings.simplefilter(action='ignore')
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer, KNNImputer # MissingIndicator
+from calc_Tadv import calc_Tadv
+import copy
+
+def multi_linear_regress(x, y, dst_dt):
+    '''
+    x shape: (nvar, ntime) --> (nvar, ntime, nlat, nlon)
+    y shape: (ntime) --> (ntime, nlat, nlon)
+    dst_dt shape: (nvar) -> (nvar, nlat, nlon)
+    '''
+    # xx should be (ntime, nvar)
+    xx = x.transpose() # np.moveaxis(x, 0, -1)
+    # https://scikit-learn.org/stable/modules/impute.html
+    #imputer = KNNImputer(n_neighbors=2, weights="uniform")
+    imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+    xx_imputed = imputer.fit_transform(xx)
+    #print(f"x: {x.shape} | xx: {xx.shape} | xx_imputed: {xx_imputed.shape} | y: {y.shape}")
+    if xx_imputed.shape[-1] == xx.shape[-1] and (not np.any(np.isnan(y))):
+        coef = LinearRegression().fit(xx_imputed, y).coef_
+    else:
+        coef = np.ones(xx.shape[-1], dtype='float32') * np.nan
+    dst_dt = coef
+    return dst_dt
+
+def CCF_multi_linear_regress(CCF_stand_anomalies_arr, y_anomaly):
+    # https://sklearn-xarray.readthedocs.io/en/latest/auto_examples/plot_linear_regression.html
+
+    shp = list(y_anomaly.shape)
+    Nx = len(CCF_stand_anomalies_arr)
+    shp.insert(0, Nx)
+    CCF_stand_anomalies = np.zeros(shp, dtype='float32')
+    for i in range(Nx):
+        CCF_stand_anomalies[i,] = CCF_stand_anomalies_arr[i]
+    
+    dims = ('ind', 'time', 'lat', 'lon')
+    ind = np.arange(0, Nx, 1)
+    coords = {}
+    for d in dims:
+        if 'ind' in d:
+            coords[d] = ind
+        else:
+            coords[d] = y_anomaly[d]
+    CCF_stand_anomalies = xr.DataArray(CCF_stand_anomalies, dims=dims, coords=coords)
+    
+    lats = y_anomaly.lat
+    lons = y_anomaly.lon
+    nlat = len(lats)
+    nlon = len(lons)
+    out_coeff = np.ones((Nx, nlat, nlon), dtype='float32') * np.nan
+    out_coeff = xr.DataArray(out_coeff, dims=('new_ind', 'lat', 'lon'), 
+                coords={'new_ind':ind, 'lat':lats, 'lon':lons})
+
+    # https://xarray.pydata.org/en/stable/examples/apply_ufunc_vectorize_1d.html
+    out_coeff2 = xr.apply_ufunc(
+            multi_linear_regress,   # first the function (x, y, dst_dt)
+            CCF_stand_anomalies,    # x for func
+            y_anomaly,              # y for func
+            out_coeff,              # dst_dt for func
+            input_core_dims=[["ind", "time"], ["time"], ['new_ind']], # list with one entry per arg
+            output_core_dims=[['new_ind']],
+            exclude_dims=set(("ind","time")),  # dimensions allowed to change size. Must be a set!
+            vectorize=True,  # loop over non-core dims
+            dask="parallelized",
+            output_dtypes=[y_anomaly.dtype],  # one per output
+        ).rename({"new_ind": "ind"})
+    return out_coeff2
+
+def sel_from_subsidence_region_tm(ds, slat=-30, nlat=30, ocn=True,
+                               land_mask_dir='./data'):
+    coeff = 3600. * 24. / 100.
+    try:
+        w500 = ds.omega.sel(pfull=500).mean('time') * coeff
+        w700 = ds.omega.sel(pfull=700).mean('time') * coeff
+    except:
+        w500 = ds.omega.interp(pfull=500).mean('time') * coeff
+        w700 = ds.omega.interp(pfull=700).mean('time') * coeff
+    lats = ds.lat
+    w0 = 10 # hPa/day
+    ind_low = (w500 >= w0) & (w700 >= w0) & (lats >= slat) & (lats <= nlat)
+
+    if ocn:
+        # get land_mask
+        lsm_fn = os.path.join(land_mask_dir, 'era_land_t42.nc')
+        ds_mask = xr.open_dataset(lsm_fn, decode_times=False)
+        ds.coords['mask'] = (('lat', 'lon'), ds_mask.land_mask.values)
+        ind_low = ind_low & (ds.mask == 0)
+
+    return ind_low
+
+def sel_cu_sc_from_subsidence_region_tm(ds, slat=-30, nlat=30, ocn=True,
+                               land_mask_dir='./data', T0=18.5, method='LTs'):
+    """
+    Medeiros, B., & Stevens, B. (2011). Climate dynamics, 36(1-2), 385-399.
+    Revealing differences in GCM representations of low clouds. 
+    doi: 10.1007/s00382-009-0694-5
+
+    Criteria:
+    1. w500 >= 10 hPa/day & w700 >= 10 hPa/day
+    2. Trade wind cumulus: LTS < 18.5; 
+       Stratocumulus: LTS >= 18.5
+    """
+    coeff = 3600. * 24. / 100.
+    try:
+        w500 = ds.omega.sel(pfull=500).mean('time') * coeff
+        w700 = ds.omega.sel(pfull=700).mean('time') * coeff
+    except:
+        w500 = ds.omega.interp(pfull=500).mean('time') * coeff
+        w700 = ds.omega.interp(pfull=700).mean('time') * coeff
+
+    lts = ds.lts.mean('time')
+    eis = ds.eis.mean('time')
+    lats = ds.lat
+
+    if 'LTS' in method.upper():
+        w0 = 10 # hPa/day
+        ind_low = (w500 >= w0) & (w700 >= w0) & (lats >= slat) & (lats <= nlat)
+        # select Cumulus and Stratocumulus
+        #T0 = 20 #18.5  # K
+        ind_Cu = ind_low & (lts < T0)
+        ind_Sc = ind_low & (lts >= T0)
+
+    if 'EIS' in method.upper():
+        w0 = 15
+        #T0 = 4
+        ind_low = (w700 >= w0) & (lats >= slat) & (lats <= nlat)
+        ind_Cu = ind_low & (eis < T0)
+        ind_Sc = ind_low & (eis >= T0)
+
+    if ocn:
+        # get land_mask
+        lsm_fn = os.path.join(land_mask_dir, 'era_land_t42.nc')
+        ds_mask = xr.open_dataset(lsm_fn, decode_times=False)
+        ds.coords['mask'] = (('lat', 'lon'), ds_mask.land_mask.values)
+        #ind_low = ind_low & (ds.mask == 0)
+        ind_Cu = ind_Cu & (ds.mask == 0)
+        ind_Sc = ind_Sc & (ds.mask == 0)
+    return ind_Cu, ind_Sc
+
+def reg_mean_coeff_from_ind(out_coeff, ind_reg):
+    reg_coeff = []
+    Nx = len(out_coeff.ind)
+    for i in range(Nx):
+        dt_ind = out_coeff.isel(ind=i).where(ind_reg, drop=True)
+        coslat = np.cos(np.deg2rad(dt_ind.lat))
+        dt_ind_mean = dt_ind.mean(('lon'), skipna=True)
+        dt_mask = np.ma.masked_array(dt_ind_mean, mask=np.isnan(dt_ind_mean))
+        dt_ind_mask_mean = np.ma.average(dt_mask, axis=0, weights=coslat)
+        reg_coeff.append(dt_ind_mask_mean)
+    return np.array(reg_coeff)
+
+def reg_mean_dt_from_ind(dt, ind_reg):
+    dt_ind = dt.where(ind_reg, drop=True)
+    coslat = np.cos(np.deg2rad(dt_ind.lat))
+    dt_ind_mean = dt_ind.mean(('time', 'lon'), skipna=True)
+    dt_mask = np.ma.masked_array(dt_ind_mean, mask=np.isnan(dt_ind_mean))
+    dt_ind_mask_mean = np.ma.average(dt_mask, axis=0, weights=coslat)
+
+    return dt_ind_mask_mean
+
+def get_final_nyr_mean(dt, n=5):
+    return dt[-12*n:, :, :].mean('time')
+
+# def get_dst_dt_from_ind(CCF_arr, cldvar, ind):
+#     #Nx = len(CCF_arr)
+#     dst_CCF_arr = []
+#     #dst_swcre_anomaly
+#     for dt in CCF_arr:
+#         dt_ind = dt.where(ind, drop=True)
+#         # shp = dt_ind.shape
+#         # new_shp = (shp[0], shp[1]*shp[2])
+#         # dst_CCF_arr.append(dt_ind.values.reshape(new_shp))
+#         dst_CCF_arr.append(dt_ind)
+
+#     # ccf_shp = (Nx, shp[0], shp[1]*shp[2])
+#     # dst_CCF = np.zeros(ccf_shp, dtype='float32')
+#     # for i in range(Nx):
+#     #     dst_CCF[i,:,:] = dst_CCF_arr[i]
+#     #dst_cldvar = cldvar.where(ind, drop=True).values.reshape(new_shp)
+#     dst_cldvar = cldvar.where(ind, drop=True)
+#     return dst_CCF_arr, dst_cldvar
+
+def get_stand_anomalies_arr(var_list, var_names, ds_era5):
+    stand_anomalies_arr = []
+    #anomalies_arr = []
+    for var, var_nm in zip(var_list, var_names):
+        var_de = detrend_dim(var, 'time', deg=1, skipna=True)
+        clim_mean = var_de.groupby("month").mean("time")
+        #clim_std = var_de.groupby("month").std("time")
+        clim_std = ds_era5[var_nm]
+        stand_anomalies = xr.apply_ufunc(lambda x, m, s: (x - m) / s,
+                        var_de.groupby("month"), clim_mean, clim_std)
+        stand_anomalies_arr.append(stand_anomalies)
+        # anomalies = var.groupby("month") - clim_mean
+        # anomalies_arr.append(anomalies)
+    return stand_anomalies_arr
+
+
+if __name__ == '__main__':
+    P = os.path.join
+    dt_dir = '../data/CCF_analysis/cu_sc_metric'
+    if not os.path.exists(dt_dir):
+        os.mkdir(dt_dir)
+    lsm_dir = '../inputs/'
+
+    # =================== Read Isca dataset ================== #
+    base_dir = '../inputs'
+    ppe_dir = P(base_dir, 'qflux_extracted_data_10yr')
+
+    exp_tbl = pd.read_csv('isca_qflux_exps_for_plots.csv', header=0)
+    exp_grps = list(exp_tbl.iloc[:, 0])
+    exps_arr = list(exp_tbl.iloc[:, 1])
+
+    #cld_type_arr = ['Cu', 'Sc']
+    cld_type = sys.argv[1] # Cu or Sc
+    proxy = sys.argv[2].upper() # ELF, or EIS
+    T0 = float(sys.argv[3])
+    if len(sys.argv)>4:
+        num_proxy = float(sys.argv[4])
+    else:
+        num_proxy = 2 # more than one proxies 
+    #if T0 > 15:
+    #    method = 'LTS'
+    #else:
+    #    method = 'EIS'
+    method = sys.argv[5].upper() # ELF, or EIS
+    print('cloud type, proxy, T0, proxy, method', cld_type, proxy, T0, num_proxy, method)
+
+    #var_names = ['SST', 'EIS', 'Tadv', r'RH$_{700}$', r'$\omega_{700}$']
+    #var_names = ['SST', 'ELF', 'Tadv', 'RH700', 'omega700']
+    if num_proxy > 2:
+        var_names = ['SST', proxy, 'Tadv', 'RH700', 'omega700']
+    else:
+        var_names = ['SST', proxy] #, 'Tadv', 'RH700', 'omega700']
+    #var_names = ['ELF', 'Tadv', 'RH700', 'omega700']
+    if 'elf' in proxy.lower():
+        proxy_var = 'ELF'
+    else:
+        proxy_var = 'eis'
+    
+    Nvar = len(var_names)
+    Ngrp = len(exp_grps)
+
+    tbl_names = ['toa_sw_cre', 'toa_lw_cre', 'toa_net_cre',
+                 'low_cld_amt', 'mid_cld_amt', 'high_cld_amt', 'tot_cld_amt',
+                 'cwp', 'low_cwp', 'mid_cwp', 'high_cwp',
+                 ] #'tauisccp',
+    file_nms= ['extracted_data_241_360.nc', 'extracted_data_601_720.nc']
+
+    # Read the stddev of CCF from ERA5
+    fn_era5 = P('../data', 'ERA5_monthly_stddev_for_CCFs.nc')
+    ds_era5_old = xr.open_dataset(fn_era5, decode_times=False)
+
+    fn = P(ppe_dir, file_nms[0].replace('.nc', '_'+ exp_grps[0]+'.nc'))
+    ds1 = xr.open_dataset(fn, decode_times=False)
+    ds_era5 = ds_era5_old.interp(lat=ds1.lat, lon=ds1.lon)
+
+    CCF_change_calculated = False
+    CCF_chg_tbl = np.zeros((Ngrp, Nvar))
+    var_chg_tbl = np.zeros((Ngrp, Nvar))
+
+    for tbl_var_nm in tbl_names:
+        print(tbl_var_nm)
+        tbl_ctrl = np.zeros((Ngrp, Nvar))
+        tbl_perturb = np.zeros((Ngrp, Nvar))
+        var_chg_tbl_real = np.zeros(Ngrp)
+
+        #for kk, (exp_grp, exp) in enumerate(zip(exp_grps[0:1], exps_arr[0:1])):
+        for kk, exp_grp in enumerate(exp_grps):
+            print(exp_grp, ': Read dataset...')
+            ds_arr = []
+            for file_nm in file_nms:
+                fn = P(ppe_dir, file_nm.replace('.nc', '_'+exp_grp+'.nc'))
+                ds = xr.open_dataset(fn, decode_times=False)
+                ds_arr.append(ds)
+
+            # Keep the time coordinates the same
+            ds_arr[1]['time'] = ds_arr[0].time
+
+            for ds in ds_arr:
+                add_datetime_info(ds)
+    
+            if 'cre' in tbl_var_nm:
+                calc_toa_cre_for_isca(ds_arr)
+            if 'cwp' in tbl_var_nm:
+                #calc_total_cwp_for_isca(ds)
+                calc_low_mid_high_cwp_for_isca(ds_arr, var_nms=[tbl_var_nm])
+
+            print('Get low/ocn index')
+            ind_Cu_ocn_ctrl, ind_Sc_ocn_ctrl = sel_cu_sc_from_subsidence_region_tm(ds_arr[0],
+                    slat=-35, nlat=35, ocn=True, land_mask_dir=lsm_dir, T0=T0, method=method)
+            ind_Cu_ocn_perturb, ind_Sc_ocn_perturb = sel_cu_sc_from_subsidence_region_tm(ds_arr[1], 
+                    slat=-35, nlat=35, ocn=True, land_mask_dir=lsm_dir, T0=T0, method=method)
+
+            #ind_Cu_arr = [ind_Cu_ocn_ctrl, ind_Cu_ocn_perturb]
+            #ind_Sc_arr = [ind_Sc_ocn_ctrl, ind_Sc_ocn_perturb]
+            if 'Cu' in cld_type:
+                ind_low_ocn_ctrl, ind_Sc_ocn_ctrl = sel_cu_sc_from_subsidence_region_tm(ds_arr[0],
+                        slat=-35, nlat=35, ocn=True, land_mask_dir=lsm_dir, T0=T0, method=method)
+                ind_low_ocn_perturb, ind_Sc_ocn_perturb = sel_cu_sc_from_subsidence_region_tm(ds_arr[1], 
+                        slat=-35, nlat=35, ocn=True, land_mask_dir=lsm_dir, T0=T0, method=method)
+            if 'Sc' in cld_type:
+                ind_Cu_ocn_ctrl, ind_low_ocn_ctrl = sel_cu_sc_from_subsidence_region_tm(ds_arr[0],
+                        slat=-35, nlat=35, ocn=True, land_mask_dir=lsm_dir, T0=T0, method=method)
+                ind_Cu_ocn_perturb, ind_low_ocn_perturb = sel_cu_sc_from_subsidence_region_tm(ds_arr[1], 
+                        slat=-35, nlat=35, ocn=True, land_mask_dir=lsm_dir, T0=T0, method=method)
+            ind_arr = [ind_low_ocn_ctrl, ind_low_ocn_perturb]
+
+            for nn, (ds, ind_low) in enumerate(zip(ds_arr, ind_arr)):
+                Tadv = calc_Tadv(ds.u_10m, ds.v_10m, ds.t_surf)
+                var_list = [ds.t_surf, ds[proxy_var], Tadv, 
+                            ds.rh.sel(pfull=700), ds.omega.sel(pfull=700)]
+                if nn == 0:
+                    var_list_ctrl = copy.deepcopy(var_list)
+                else:
+                    var_list_perturb = copy.deepcopy(var_list)
+
+                stand_anomalies_arr = get_stand_anomalies_arr(var_list, var_names, ds_era5)
+                tbl_var = ds[tbl_var_nm]
+                if 'cwp' in tbl_var_nm:
+                    tbl_var = tbl_var * 1e3
+                clim_mean = tbl_var.groupby("month").mean("time")
+                tbl_var_anomaly = tbl_var.groupby("month") - clim_mean
+
+                print('Get multivariate linear regression coeff')
+                out_coeff = CCF_multi_linear_regress(stand_anomalies_arr, tbl_var_anomaly)
+
+                reg_coeff = reg_mean_coeff_from_ind(out_coeff, ind_low)
+                if nn==0:
+                    tbl_ctrl[kk,:] = reg_coeff
+                else:
+                    tbl_perturb[kk,:] = reg_coeff
+
+            #===================== Changes of CCF response to warming =============== #
+            if not CCF_change_calculated:
+                print('Calculate the CCF changes per K')
+                diff_var_list = [ d2-get_final_nyr_mean(d1) for (d1, d2) in zip(var_list_ctrl, var_list_perturb) ]
+                diff_ts = ds_arr[1].temp_2m - get_final_nyr_mean(ds_arr[0].temp_2m)
+                coslat = np.cos(np.deg2rad(diff_ts.lat))
+                diff_ts_gm = np.average(diff_ts.mean(('time', 'lon')), axis=0, weights=coslat)
+
+                # Calculate how many sigma (from ctrl) it has changed
+                CCF_chg_perK_arr = []
+                for var, dt_old, var_nm in zip(diff_var_list, var_list_ctrl, var_names):
+                    clim_std_old = ds_era5[var_nm] #dt_old.groupby("month").std("time")
+                    n_sigma = xr.apply_ufunc(lambda x, s: x / s,
+                                    var.groupby("month"), clim_std_old)
+                    n_sigma_norm = n_sigma / diff_ts_gm
+                    n_sigma_norm = xr.where(n_sigma_norm==np.inf, np.nan, n_sigma_norm)
+                    n_sigma_norm = xr.where(n_sigma_norm==-np.inf, np.nan, n_sigma_norm)
+                    CCF_chg_perK_arr.append(n_sigma_norm)
+
+                # for var in CCF_chg_perK_arr:
+                #     print(get_gm(var))
+                ind_low_ocn = ind_low_ocn_ctrl & ind_low_ocn_perturb
+                CCF_chg_tbl[kk,:] = np.array([reg_mean_dt_from_ind(v, ind_low_ocn)
+                                                for v in CCF_chg_perK_arr])
+                print(CCF_chg_tbl[kk,:])
+
+            # ==================== Calculate the Var change per K (similar to Feedback) =====================#
+            print('Calculate the Var change per K (similar to Feedback')
+            tbl_var_chg_arr = []
+            for jj, CCF_chg_perK in enumerate(CCF_chg_perK_arr):
+                tbl_var_chg = out_coeff.isel(ind=jj) * CCF_chg_perK
+                tbl_var_chg_arr.append(tbl_var_chg)
+
+            ind_low_ocn = ind_low_ocn_ctrl & ind_low_ocn_perturb
+            var_chg_tbl[kk,:] = np.array([reg_mean_dt_from_ind(v, ind_low_ocn)
+                                                for v in tbl_var_chg_arr])
+            print(var_chg_tbl[kk,:])
+            delta_tbl_var = (ds_arr[1][tbl_var_nm] - get_final_nyr_mean(ds_arr[0][tbl_var_nm])) / diff_ts_gm
+            if 'cwp' in tbl_var_nm:
+                delta_tbl_var = delta_tbl_var * 1e3
+            var_chg_tbl_real[kk] = reg_mean_dt_from_ind(delta_tbl_var, ind_low_ocn)
+
+        file_name = P(dt_dir, tbl_var_nm+'_CCF_ctrl_'+cld_type+'_'+proxy+'.csv')
+        pdtbl = pd.DataFrame(data=tbl_ctrl, index=exp_grps, columns=var_names)
+        pdtbl.to_csv(file_name, header=True, index=True, float_format="%.8f")
+        print(file_name, 'saved.')
+
+        file_name = P(dt_dir, tbl_var_nm+'_CCF_perturb_'+cld_type+'_'+proxy+'.csv')
+        pdtbl = pd.DataFrame(data=tbl_perturb, index=exp_grps, columns=var_names)
+        pdtbl.to_csv(file_name, header=True, index=True, float_format="%.8f")
+        print(file_name, 'saved.')
+
+        if not CCF_change_calculated:
+            CCF_change_calculated = True
+            file_name = P(dt_dir, 'CCF_change_perK_'+cld_type+'_'+proxy+'.csv')
+            pdtbl = pd.DataFrame(data=CCF_chg_tbl, index=exp_grps, columns=var_names)
+            pdtbl.to_csv(file_name, header=True, index=True, float_format="%.8f")
+            print(file_name, 'saved.')
+
+        file_name = P(dt_dir, tbl_var_nm+'_chg_per_K_'+cld_type+'_'+proxy+'.csv')
+        pdtbl = pd.DataFrame(data=var_chg_tbl, index=exp_grps, columns=var_names)
+        pdtbl.to_csv(file_name, header=True, index=True, float_format="%.8f")
+        print(file_name, 'saved.')
+
+        file_name = P(dt_dir, tbl_var_nm+'_chg_per_K_real_'+cld_type+'_'+proxy+'.csv')
+        pdtbl = pd.DataFrame(data=var_chg_tbl_real, index=exp_grps, columns=[tbl_var_nm])
+        pdtbl.to_csv(file_name, header=True, index=True, float_format="%.8f")
+        print(file_name, 'saved.')
+
+    print('Done')
---
+++ scripts/plot_CCF_Cu_Sc_single_sum_metric.py
@@ -0,0 +1,187 @@
+import matplotlib
+matplotlib.use('Agg')
+import os
+import sys
+import numpy as np
+import xarray as xr
+import pandas as pd
+import proplot as plot
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore')
+from matplotlib.patches import Patch
+from analysis_functions import get_unique_line_labels
+
+if __name__ == '__main__':
+    P = os.path.join
+    fig_dir = '../figs/'
+    if not os.path.exists(fig_dir):
+        os.mkdir(fig_dir)
+
+    exp_tbl = pd.read_csv('isca_qflux_exps_for_plots.csv', header=0)
+    exp_grps = list(exp_tbl.iloc[:, 0])
+    markers = list(exp_tbl.iloc[:, 2])
+
+    tbl_names = ['toa_sw_cre', 'toa_lw_cre', 'toa_net_cre',
+                'low_cld_amt', 'mid_cld_amt', 'high_cld_amt', 'tot_cld_amt',
+                'cwp', 'low_cwp', 'mid_cwp', 'high_cwp',
+                #'tauisccp',
+                ]
+    tbl_titles = ['SWCRE', 'LWCRE', 'Net CRE',
+                 'low cldamt', 'mid cldamt', 'high cldamt', 'total cldamt',
+                 'CWP', r'CWP$_{low}$', r'CWP$_{mid}$', r'CWP$_{high}$',
+                 #'optical depth'
+                ]
+    units_arr1 = [r'W m$^{-2}$'] * 3 + ['%'] * 4 + [r'g m$^{-2}$'] * 4 #+ ['']
+
+    # tbl_names = ['low_cld_amt']
+    # tbl_titles = ['low cldamt']
+    # units_arr1 = ['%']
+
+    proxy = sys.argv[1].upper() # ELF, or EIS
+    #proxy = 'EIS'
+    cld_type_arr = ['All', 'Cu', 'Sc']
+    for tbl_nm, tbl_title, units in zip(tbl_names, tbl_titles, units_arr1):
+        print(proxy, tbl_nm)
+        tbl_dict = {}
+        keys = ['Var per CCF sigma', 'CCF sigma per K', 'Var per K']
+        for key in keys:
+            tbl_dict[key] = {}
+        real_chg_tbl = {}
+        for cld_type in cld_type_arr:
+            if 'all' in cld_type.lower():
+                dt_dir = '../data/CCF_analysis_metric'
+                file_nms = [tbl_nm+'_CCF_ctrl_'+proxy+'.csv',
+                        'CCF_change_perK_'+proxy+'.csv',
+                        tbl_nm+'_chg_per_K_'+proxy+'.csv']
+                file_real_nm = tbl_nm + '_chg_per_K_real_'+proxy+'.csv'
+            else:
+                dt_dir = '../data/CCF_analysis/cu_sc_metric'
+                file_nms = [tbl_nm+'_CCF_ctrl_'+cld_type+'_'+proxy+'.csv',
+                        'CCF_change_perK_'+cld_type+'_'+proxy+'.csv',
+                        tbl_nm+'_chg_per_K_'+cld_type+'_'+proxy+'.csv']
+                file_real_nm = tbl_nm + '_chg_per_K_real_'+cld_type+'_'+proxy+'.csv'
+            for key, file_nm in zip(keys, file_nms):
+                tbl = pd.read_csv(P(dt_dir, file_nm), index_col=0)
+                tbl_dict[key][cld_type] = tbl
+            real_chg_tbl[cld_type] = pd.read_csv(P(dt_dir, file_real_nm), index_col=0)
+
+        titles = [tbl_title + ' sensitivity to CCFs',
+                'Temperature-mediated change in CCFs',
+                'CCF-driven '+ tbl_title +' changes']
+        units_arr = [units + r' $\sigma^{-1}$', r'$\sigma$ K$^{-1}$ ', units + r' K$^{-1}$']
+
+        plt.close()
+        fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(4.5,6))
+
+        box_colors = ['palegreen', 'tan', 'lightblue']
+        lines = []
+        box_width = 0.4
+        xspace = box_width * len(cld_type_arr) + 0.2
+
+        # https://matplotlib.org/stable/gallery/statistics/boxplot.html
+        flierprops = dict(markeredgecolor='gray', marker='o', markersize=2) # markerfacecolor=None,
+        boxprops = dict(linewidth=0.5, color='k') #gray')
+        medianprops = dict(linestyle='--', linewidth=0.5, color='k') #boxprops
+        whiskerprops = dict(linewidth=0.5, color='k')
+        capprops = whiskerprops
+        #xlim = [-2.4, 0.5+(3*box_width+0.2)*5]
+
+        for ff, (ax, key, title, units) in enumerate(zip(axes, keys, titles, units_arr)):
+            for cc, (cld_type, box_color) in enumerate(zip(cld_type_arr, box_colors)):
+                xticks = []
+                tbl = tbl_dict[key][cld_type]
+                row_names = tbl.index
+                col_names = tbl.columns
+                '''
+                xlocs = [-box_width, 0, box_width]
+                for i, col_nm in enumerate(col_names):
+                    l = ax.boxplot(tbl[col_nm], positions=[2*i+xlocs[cc]], patch_artist=True, label=)
+                    lines.extend(l)
+                    for patch, color in zip(l['boxes'], [box_color]):
+                        patch.set_facecolor(color)
+                '''
+                xlocs = []
+                labels = []
+                colors = []
+                xlocs1 = [-box_width, 0, box_width]
+
+                for i, col_nm in enumerate(col_names):
+                    if i == 0:
+                        x = 1
+                    else:
+                        x = x + xspace #2 * i + 1
+                    xticks.append(x)
+                    xlabels1 = ['', col_nm, '']
+                    xlocs.append(x + xlocs1[cc])
+                    labels.append(xlabels1[cc])
+                    colors.append(box_color)
+                widths = [box_width] * len(col_names)
+                b = ax.boxplot(tbl.T, positions=xlocs, widths=widths, patch_artist=True,
+                        labels=labels, boxprops=boxprops, medianprops=medianprops,
+                        whiskerprops=whiskerprops, capprops=capprops, flierprops=flierprops)
+                for patch, color in zip(b['boxes'], colors):
+                    patch.set_facecolor(color)
+
+                if 'Var per K' in key:
+                    # == For the predicted changes == #
+                    predicted = np.zeros(len(row_names))
+                    for kk, row_nm  in enumerate(row_names):
+                        for i, col_nm in enumerate(col_names):
+                            predicted[kk] = predicted[kk] + tbl[col_nm][row_nm]
+                    x1 = 1 - xspace
+                    xticks.insert(0, x1)
+                    xlocs = [x1 + xlocs1[cc]]
+                    xlabels1 = ['', 'Predicted', '']
+                    labels = [xlabels1[cc]]
+                    b = ax.boxplot(predicted, positions=xlocs, widths=[box_width], patch_artist=True,
+                            labels=labels, boxprops=boxprops, medianprops=medianprops,
+                            whiskerprops=whiskerprops, capprops=capprops, flierprops=flierprops)
+                    for patch, color in zip(b['boxes'], colors):
+                        patch.set_facecolor(color)
+
+                    # == For the actual changes == #
+                    x2 = x1 - xspace
+                    xticks.insert(0, x2)
+                    xlocs = [x2 + xlocs1[cc]]
+                    xlabels1 = ['', 'Sum', '']
+                    labels = [xlabels1[cc]]
+                    b = ax.boxplot(real_chg_tbl[cld_type].T, positions=xlocs, widths=[box_width],
+                        patch_artist=True, labels=labels, boxprops=boxprops, medianprops=medianprops,
+                        whiskerprops=whiskerprops, capprops=capprops, flierprops=flierprops)
+                    for patch, color in zip(b['boxes'], colors):
+                        patch.set_facecolor(color)
+                    ax.axvline(x=x1+(1-x1)/2, linewidth=0.5, linestyle='--', color='gray')
+
+            ax.xaxis.set_tick_params(which='minor', bottom=False)
+            #ax.set_xlim(xlim)
+            ax.set_ylabel(units)
+            ax.set_title('('+chr(97+ff)+') '+title) #, fontweight='bold')
+            
+            if len(col_names)>2:
+                xticklabels1 = ['Actual', 'Predicted', 'SST', proxy, r'$T_{adv}$', r'RH$_{700}$', r'$\omega_{700}$']
+            else:
+                xticklabels1 = ['Actual', 'Predicted', 'SST', proxy] #, r'$T_{adv}$', r'RH$_{700}$', r'$\omega_{700}$']
+            if ff == 2:
+                xticklabels = xticklabels1[0:]
+                ax.set_xticks(xticks)
+                ax.set_xticklabels(xticklabels)
+            else:
+                xticklabels = xticklabels1[2:]
+                ax.set_xticks(xticks)
+                ax.set_xticklabels(xticklabels)
+            ax.grid(b=False)
+        for ax in axes:
+            xlim = [min(xticks) - xspace / 2, max(xticks) + xspace / 2]
+            ax.set_xlim(xlim)
+            ax.plot(xlim, [0,0], ':', color='gray', lw=0.5)
+        legend_elements = []
+        for cld_type, color in zip(cld_type_arr, box_colors):
+            legend_elements.append(Patch(facecolor=color, edgecolor=color, label=cld_type))
+        # Need to add handles=xxx
+        axes[0].legend(handles=legend_elements, loc='upper left', ncol=1)
+        fig.tight_layout()
+        fig_name = P(fig_dir, 'CCF_analysis_for_diff_cld_types_'+tbl_nm+'_'+proxy+'.pdf')
+        fig.savefig(fig_name, bbox_inches='tight', pad_inches=0.1)
+        #plt.show()
+        print(fig_name, 'saved')
