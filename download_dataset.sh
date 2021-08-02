b725fb48d417efff93084dee18124728ce139082
Qun Liu <liuqunxyz@gmail.com>
2021-08-02 19:52:47 +0100

    download observed clisccp data

--- download_dataset.sh
+++ download_dataset.sh
@@ -19,11 +19,14 @@ fn_arr=("cmip56_forcing_feedback_ecs.json" \
         "cmip5_amip4K_cld_fbks.json" \
         "cmip5_amip_cld_errs.json" \
         "cmip6_amip-p4K_cld_fbks.json" \
-        "cmip6_amip_cld_errs.json")
+        "cmip6_amip_cld_errs.json" \
+        "AC_clisccp_wap_ISCCP_HGG_198301-200812.nc" \
+        "AC_clisccp_ISCCP_HGG_198301-200812.nc")
 for fn in "${fn_arr[@]}"
 do
     wget -c -N --directory-prefix=$dt_dir https://raw.githubusercontent.com/mzelinka/assessed-cloud-fbks/master/data/$fn
 done
+ln -s $outdir/cloud_kernels2.nc $dt_dir/
 
 echo "Download ISCCP observation data" 
 wget -c -N --directory-prefix=$outdir --no-check-certificate https://climserv.ipsl.polytechnique.fr/cfmip-obs/data/ISCCP/clisccp_198307-200806.nc
---
+++ inputs/zelinka_data/cloud_kernels2.nc
@@ -0,0 +1,1 @@
+./inputs//cloud_kernels2.nc
